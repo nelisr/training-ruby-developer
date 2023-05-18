a755377cae519507fed5d348ac5eee8960ff334d
Nelis Rodrigues <nelis.rodriguess@gmail.com>
2023-05-18 11:42:49 -0300

    solved first challenge

---
+++ 01_challenge.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Create an program that request the firstname, lastname and age of a user, after,
+# print with output the data interpolated
+
+print "Digit your First Name: "
+first_name = gets.chomp.to_s
+
+print "Digit your Last Name: "
+last_name = gets.chomp.to_s
+
+print "Digit your Age: "
+age = gets.chomp.to_s
+
+puts "Your full name is #{first_name} #{last_name} and your age is: #{age}"
